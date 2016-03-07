3b47f87f9bcee325488c8a374dca3618688d006c
Alexander Zagovorichev <zagovorichev@gmail.com>
2016-03-08 00:34:28 +0300

    add routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,16 @@
 <?php
 /**
  *
+ * Load all routes from extensions or use in current context, or full path for other context
+ *
  * @author Alexander Zagovorichev <zagovorichev@gmail.com>
  */
+
+Route::get('test', function(){
+    echo '1';
+});
+
+Route::resource('posts', 'PostsController');
+
+Route::get('/', 'MainController@index');
+
