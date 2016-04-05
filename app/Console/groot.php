52fa9a43aaa8b27b967c3e45c74673b5519f4f01
Alexander Zagovorichev <zagovorichev@1pt.com>
2016-04-05 12:27:08 +0300

    construct arch

--- app/Console/groot.php
+++ app/Console/groot.php
@@ -1,8 +1,8 @@
-#!/usr/bin/env php
 <?php
 
-require_once __DIR__ . "/../../../vendor/autoload.php";
+require_once dirname(dirname(dirname(__DIR__))) . DIRECTORY_SEPARATOR . 'app.php';
 
-$application = new \Symfony\Component\Console\Application();
+$application = new \Symfony\Component\Console\Application('Blog-Tree console', '0.0.1');
 $application->add(new \bt\btRoot\app\Console\Commands\InstallCommand());
+$application->add(new \bt\Garden\Foundation\Console\ServeCommand());
 $application->run();
