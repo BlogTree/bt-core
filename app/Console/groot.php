958137b2dc00dbd6bc6d6b5ade4ff533f7a557ac
Alexander Zagovorichev <zagovorichev@1pt.com>
2016-04-05 16:38:19 +0300

    fixes (remove vendor dependencies on unused package)

--- app/Console/groot.php
+++ app/Console/groot.php
@@ -5,4 +5,5 @@ require_once __DIR__ . "/../../../vendor/autoload.php";
 
 $application = new \Symfony\Component\Console\Application();
 $application->add(new \bt\btRoot\app\Console\Commands\InstallCommand());
+$application->add(new \bt\Garden\Foundation\Console\ServeCommand());
 $application->run();
